f326616dd86d2826df88bf965941b4a399348390
MythicalRev <landonsciara@icloud.com>
2026-02-10 16:55:15 -0500

    update readme and more cleanup (yes im procrastinating shut up)

--- includes/modifies/LevelInfoLayerModify.hpp
+++ includes/modifies/LevelInfoLayerModify.hpp
@@ -1,9 +1,6 @@
 #pragma once
 
-#include "Geode/utils/async.hpp"
 #include <Geode/Geode.hpp>
-#include <Geode/utils/web.hpp>
-#include <Geode/utils/async.hpp>
 #include "../custom/PauseChecker.hpp"
 #include "../custom/PlayPauseHandler.hpp"
 
@@ -11,13 +8,6 @@ using namespace geode::prelude;
 
 #include <Geode/modify/LevelInfoLayer.hpp>
 class $modify(SpotiLevelInfoLayer, LevelInfoLayer) {
-    struct Fields {
-        TaskHolder<web::WebResponse> m_listener;
-
-        std::string m_apiEndpoint = "https://api.spotify.com/v1/me/player/pause";
-        std::string m_accessToken = Mod::get()->getSettingValue<std::string>("accesstoken");
-    };
-
     bool init(GJGameLevel* level, bool challenge) {
         if (!LevelInfoLayer::init(level, challenge)) return false;
 
--- includes/modifies/PlayLayerModify.hpp
+++ includes/modifies/PlayLayerModify.hpp
@@ -1,20 +1,12 @@
 #pragma once
 
 #include <Geode/Geode.hpp>
-#include <Geode/utils/web.hpp>
 #include "../custom/PlayPauseHandler.hpp"
 
 using namespace geode::prelude;
 
 #include <Geode/modify/PlayLayer.hpp>
 class $modify(SpotiPlayLayer, PlayLayer) {
-    struct Fields {
-        TaskHolder<web::WebResponse> m_listener;
-
-        std::string m_apiEndpoint = "https://api.spotify.com/v1/me/player/play";
-        std::string m_accessToken = Mod::get()->getSettingValue<std::string>("accesstoken");
-    };
-
     bool init(GJGameLevel* level, bool useReplay, bool dontCreateObjects) {
         if (!PlayLayer::init(level, useReplay, dontCreateObjects)) return false;
         
